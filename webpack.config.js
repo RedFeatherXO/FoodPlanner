0a5c6e33f784487ef4ce673cc248006b1953b65c
Meik -- <meikklein123@gmail.com>
2024-08-02 09:39:43 +0200

    test

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, "dist"),
+      },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+};
